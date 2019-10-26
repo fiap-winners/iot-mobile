c38d2b125e695b2c035aa7f32b231224dce38c1c
johelo <nriqlops@gmail.com>
2019-10-26 17:13:48 -0300

    Beacon images from font awesome.

--- app/src/main/java/br/com/fiap/trackmed/DetectionActivity.kt
+++ app/src/main/java/br/com/fiap/trackmed/DetectionActivity.kt
@@ -5,19 +5,41 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.os.RemoteException
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detection.*
 import org.altbeacon.beacon.*
-import java.text.DecimalFormat
-import kotlin.math.roundToLong
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class DetectionActivity : AppCompatActivity(), BeaconConsumer {
 
     private var beaconManager: BeaconManager? = null
     private val PERMISSION_REQUEST_COARSE_LOCATION = 1
+
+    private val TRACKMED_BEACON = "aa"
+    private val TYPE_BED = "00"
+    private val TYPE_FOUNTAIN = "01"
+    private val TYPE_HYGIENIZING_DISPENSER = "02"
+    private val TYPE_BALCONY = "03"
+    private val TYPE_VENDING_MACHINE = "04"
+    private val TYPE_COMPUTER = "05"
+    private val TYPE_CABINET = "06"
+    private val TYPE_LOCKERS = "07"
+    private val TYPE_BULLETIN_BOARD = "08"
+    private val TYPE_TIME_ATTENDANCE_REGISTER = "09"
+    private val TYPE_MACHINE_01 = "0a"
+    private val TYPE_MACHINE_02 = "0b"
+    private val TYPE_MACHINE_03 = "0c"
+
+    private val MIN_SECONDS_TO_RECORD = 2
+    private val MAX_DISTANCE_TO_SHOW_DETECTION = 10
+
+    private var lastSeenDateTime: Date = Date()
+    private var lastSeenBeaconId: String = ""
+    private var lastSeenBeaconDistance: Double = 0.0
+    private var lastBeaconIdRecorded: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -80,29 +102,73 @@ class DetectionActivity : AppCompatActivity(), BeaconConsumer {
                 if (beacons.isNotEmpty()) {
 
 //                    val firstBeacon = beacons.iterator().next()
-                    val firstBeacon = beacons.minBy { it.distance }
-                    runOnUiThread {
-                        updateDistance(firstBeacon!!)
+                    val firstBeacon: Beacon? = beacons.filter { it.id2.toString().substring(2,4) == TRACKMED_BEACON }.minBy { it.distance }
+                    if (firstBeacon != null) {
+                        runOnUiThread {
+                            updateDistance(firstBeacon!!)
+                        }
                     }
                 }
             }
         })
         try {
-            beaconManager!!.startRangingBeaconsInRegion(Region("Hostpital", null, null, null))
+            beaconManager!!.startRangingBeaconsInRegion(Region("TrackMedRegion", null, null, null))
         } catch (e: RemoteException) {
         }
     }
 
     fun updateDistance(beacon: Beacon) {
 
-        if (beacon.distance > 10.0) {
-            imageBeacon.visibility = View.INVISIBLE
-            textDistance.text = "nada"
-            textBeaconId.text = "nada"
+        if (beacon.id1.toString() != lastSeenBeaconId) {
+            lastSeenBeaconId = beacon.id1.toString()
+            lastSeenDateTime = Date()
+            lastSeenBeaconDistance = beacon.distance
+
+            if (lastSeenBeaconDistance > MAX_DISTANCE_TO_SHOW_DETECTION) {
+                imageBeacon.visibility = View.INVISIBLE
+                textDistance.text = "nada"
+                textBeaconId.text = "nada"
+            } else {
+                imageBeacon.text = getString(getBeaconImage(beacon.id2.toString().substring(4, 6)))
+                imageBeacon.visibility = View.VISIBLE
+                textDistance.text = beacon.distance.toString()
+                textBeaconId.text = beacon.id2.toString().substring(4, 6)
+            }
         } else {
-            imageBeacon.visibility = View.VISIBLE
-            textDistance.text = beacon.distance.toString()
-            textBeaconId.text = beacon.toString()
+
+            if (lastSeenBeaconId == lastBeaconIdRecorded) {
+                if (beacon.distance >= MAX_DISTANCE_TO_SHOW_DETECTION &&
+                    TimeUnit.MILLISECONDS.toSeconds(Date().time - lastSeenDateTime.time) < MIN_SECONDS_TO_RECORD
+                ) {
+
+                    // Persistir
+                    lastBeaconIdRecorded = beacon.id1.toString()
+                } else {
+                    lastSeenBeaconId = ""
+                }
+            }
+        }
+
+        textLastSeenDateTime.text = lastSeenDateTime.toString()
+    }
+
+    fun getBeaconImage(beaconType: String): Int {
+        return when(beaconType) {
+            TYPE_BED -> R.string.fa_procedures_solid
+            TYPE_FOUNTAIN -> R.string.fa_water_solid
+            TYPE_HYGIENIZING_DISPENSER -> R.string.fa_hand_holding_solid
+            TYPE_BALCONY -> R.string.fa_clinic_medical_solid
+            TYPE_VENDING_MACHINE -> R.string.fa_store_solid
+            TYPE_COMPUTER -> R.string.fa_laptop_medical_solid
+            TYPE_CABINET -> R.string.fa_archive_solid
+            TYPE_LOCKERS -> R.string.fa_adn
+            TYPE_BULLETIN_BOARD -> R.string.fa_chalkboard_solid
+            TYPE_TIME_ATTENDANCE_REGISTER -> R.string.fa_user_clock_solid
+            TYPE_MACHINE_01 -> R.string.fa_syringe_solid
+            TYPE_MACHINE_02 -> R.string.fa_charging_station_solid
+            TYPE_MACHINE_03 -> R.string.fa_atom_solid
+            else -> R.string.fa_ban_solid
         }
     }
+
 }
