073f91733019a027515350be444fc69fa019862f
johelo <nriqlops@gmail.com>
2019-10-27 13:58:45 -0300

    Splashscreen added

--- app/src/main/java/br/com/fiap/trackmed/MainActivity.kt
+++ app/src/main/java/br/com/fiap/trackmed/MainActivity.kt
@@ -1,12 +1,39 @@
 package br.com.fiap.trackmed
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000 //3 seconds
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, DetectionActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mDelayHandler = Handler()
+
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+    }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
     }
 }
